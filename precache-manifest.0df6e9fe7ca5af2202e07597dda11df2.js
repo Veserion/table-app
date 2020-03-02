8a761f5389b4c7e4025992871e26eda9642fde57
Daniil <workstation@MacBook-Pro-Daniil.local>
2020-03-02 15:48:07 +0300

    Updates

---
+++ precache-manifest.0df6e9fe7ca5af2202e07597dda11df2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5871da60157ca1b1caffce2bba56c26d",
+    "url": "/table-app/index.html"
+  },
+  {
+    "revision": "49bdb4c196dd6fbc9e05",
+    "url": "/table-app/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "49bdb4c196dd6fbc9e05",
+    "url": "/table-app/static/js/2.98a7dce0.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/table-app/static/js/2.98a7dce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8f698416c270bfe2b6",
+    "url": "/table-app/static/js/main.c353b87e.chunk.js"
+  },
+  {
+    "revision": "3f2df0b1189bd3741d35",
+    "url": "/table-app/static/js/runtime-main.3093c7e6.js"
+  }
+]);
